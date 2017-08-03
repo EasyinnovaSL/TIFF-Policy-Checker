4b0379f96eac1e95bd60e02b2e78d7bd9ba723e8
Victor Muñoz <victormunoz@easyinnova.com>
2017-08-03 08:35:49 +0200

    License fix

--- src/main/java/com/easyinnova/policy_checker/Main.java
+++ src/main/java/com/easyinnova/policy_checker/Main.java
@@ -9,7 +9,7 @@
  * <p> You should have received a copy of the GNU General Public License and the Mozilla Public
  * License along with this program. If not, see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>
  * and at <a href="http://mozilla.org/MPL/2.0">http://mozilla.org/MPL/2.0</a> . </p> <p> NB: for the
- * © statement, include Easy Innova SL or other company/Person contributing the code. </p> <p> ©
+ *  statement, include Easy Innova SL or other company/Person contributing the code. </p> <p>
  * 2015 Easy Innova, SL </p>
  */
 
